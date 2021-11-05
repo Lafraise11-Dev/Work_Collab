fb11096a78983191cfc9043f2d49b86ecd4bf553
Leonard Huỳnh <90182360+LeonardCpp@users.noreply.github.com>
2021-11-05 15:00:34 +0700

    Update bai1Th.cpp

--- lab3/bai1Th.cpp
+++ lab3/bai1Th.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int tong(int n)
+{
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 4 == 0 && i % 5 != 0)
+            sum += i;
+    }
+    return sum;
+}
+
 int main()
 {
-  int n;
-  cout << "Nhap n: "; cin >> n;
+    int n;
+    cout << "Nhap n: "; cin >> n;
+    cout << "Tong cac so chia het cho 4 va khong chia het cho 5 nho hon " << n << " la: " << tong(n);
+    return 0;
 }
