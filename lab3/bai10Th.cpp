6283d2046ead11b08d08e91fa739397beaf6f052
LaFraise <90182360+LeonardCpp@users.noreply.github.com>
2021-11-06 23:24:06 +0700

    Create bai10Th.cpp

---
+++ lab3/bai10Th.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    for(int i = 5; i <= 30; i+=5)
+    {
+        cout << "Luong calo dot duoc sau " << i << " phut chay bo: " << i * 3.6 << endl;
+    }
+    return 0;
+}
+
+
+Luong calo dot duoc sau 5 phut chay bo: 18
+Luong calo dot duoc sau 10 phut chay bo: 36
+Luong calo dot duoc sau 15 phut chay bo: 54
+Luong calo dot duoc sau 20 phut chay bo: 72
+Luong calo dot duoc sau 25 phut chay bo: 90
+Luong calo dot duoc sau 30 phut chay bo: 108
